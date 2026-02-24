7f551050e93eaab17fab076a3e8eaaeffa99ff2b
Kythhiwa <maksvolk756@gmail.com>
2026-02-24 19:27:47 +0300

    graph_builder_first_step

---
+++ src/graph_builder.py
@@ -0,0 +1,8 @@
+
+def read_sql(topic: str, request_id: int):
+    # Читает данные из sql и отдает их в build_graph
+    pass
+
+def build_graph(sql_data):
+    # Превращает данные из сырого текста в связный граф
+    pass
