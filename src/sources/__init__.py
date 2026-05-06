a30e248c548713eaa939234688bb3b2d28788d24
Tim <timdim26@mail.ru>
2026-05-06 17:46:35 +0300

    правки после слияния

--- src/sources/__init__.py
+++ src/sources/__init__.py
@@ -2,6 +2,5 @@ from .github import fetch_github
 from .open_alex import fetch_open_alex
 from .wikidata import fetch_wikidata
 from .wikipedia import fetch_wikipedia
-from .github import fetch_wikidata
 
-__all__ = ["fetch_wikidata", "fetch_semantic_scholar", "fetch_open_alex", "fetch_wikipedia"]
+__all__ = ["fetch_wikidata", "fetch_github", "fetch_open_alex", "fetch_wikipedia"]
